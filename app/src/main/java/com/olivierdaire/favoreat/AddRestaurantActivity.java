c5363d1bcd1587f9d79573c4d1b7794e62d54e4d
BettinaMigot <betti-my@hotmail.fr>
2016-05-01 21:08:04 +0200

    Add data storage

--- app/src/main/java/com/olivierdaire/favoreat/AddRestaurantActivity.java
+++ app/src/main/java/com/olivierdaire/favoreat/AddRestaurantActivity.java
@@ -1,25 +1,48 @@
 package com.olivierdaire.favoreat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.location.Geocoder;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
 
 
-public class AddRestaurantActivity extends AppCompatActivity implements OnMapReadyCallback  {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class AddRestaurantActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private TextView textView;
     private SeekBar seekBar;
@@ -54,9 +77,11 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnMapRea
                 progress = progresValue;
                 textView.setText(progress + " $");
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
@@ -80,8 +105,8 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnMapRea
             case R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
-            case R.id.action_sort:
-                // TODO Show dialog and sort restaurants
+            case R.id.validate:
+                onClickValidate();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -95,7 +120,68 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnMapRea
         LocationService locationService = new LocationService(AddRestaurantActivity.this);
         LatLng latLng = new LatLng(locationService.getLatitude(), locationService.getLongitude());
 
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Your current position"));
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses  = geocoder.getFromLocation(locationService.getLatitude(),locationService.getLongitude(), 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMap.addMarker(new MarkerOptions().position(latLng).title(addresses.get(0).getAddressLine(0)));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18.0f));
+
+        final List<Address> finalAddresses = addresses;
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener()
+        {
+
+            @Override
+            public boolean onMarkerClick(Marker arg0) {
+                TextView restAdress = (TextView) findViewById(R.id.RestaurantAdress);
+                restAdress.setText(finalAddresses.get(0).getAddressLine(0));
+                return true;
+            }
+
+        });
+
     }
+
+    public void onClickValidate() {
+        // Get views by ID
+        EditText editName = (EditText) findViewById(R.id.RestaurantName);
+        EditText editAddress = (EditText) findViewById(R.id.RestaurantAdress);
+        EditText editCuisine = (EditText) findViewById(R.id.RestaurantCuisine);
+        TextView editPrice = (TextView) findViewById(R.id.textPrice);
+        String price = editPrice.getText().toString().replaceAll("[^0-9]", "");
+        RatingBar editRate = (RatingBar) findViewById(R.id.RestaurantNote);
+
+        // Get the latitude and longitude from address
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        double latitude = 0;
+        double longitude = 0;
+        try {
+            address = coder.getFromLocationName(editAddress.getText().toString(), 5);
+            Address location = address.get(0);
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Create new restaurant object
+        Restaurant newRestaurant = new Restaurant(editName.getText().toString(), latitude, longitude, editCuisine.getText().toString(), Integer.parseInt(price), Math.round(editRate.getRating()));
+
+        // Save de new restaurant in shared preference file
+        SharedPreferences appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        SharedPreferences.Editor prefsEditor = appSharedPrefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(newRestaurant);
+        prefsEditor.putString(newRestaurant.getName(), json);
+        prefsEditor.commit();
+
+        // Return to main activity
+        Intent returnBtn = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(returnBtn);
+    }
+
 }
